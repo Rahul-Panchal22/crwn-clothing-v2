6ed93c02ebf9dcf382547b5b1a18a83c71ae7a99
Rahul Panchal <rahulpydev22@gmail.com>
2022-04-24 11:23:27 +0530

    updating our directory pag with directory component and category item component

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,20 @@
+import "./category-item.styles.scss";
+
+const CategoryItem = (props) => {
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${props.category.imageUrl})`,
+        }}
+      />
+      <div className="category-body-container">
+        <h2 className="title">{props.category.title.toUpperCase()}</h2>
+        <p className="sub-title">SHOP NOW</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
